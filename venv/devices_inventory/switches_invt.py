706bd52ccf5779766bb1a442bdcddf959c919d73
ndeva <ndevabn@gmail.com>
2021-01-07 00:31:31 +0300

    Switches inventory in switches_invt.py

--- venv/devices_inventory/switches_invt.py
+++ venv/devices_inventory/switches_invt.py
@@ -2,33 +2,33 @@ core_S1 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.71',
     'username': 'brian',
-    'password': 'cisco',
+    'password': 'c1sc0',
 }
 
 core_S2 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.72',
     'username': 'brian',
-    'password': 'cisco',
+    'password': 'c1sc0',
 }
 
 access_S3 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.73',
     'username': 'brian',
-    'password': 'cisco',
+    'password': 'c1sc0',
 }
 
 access_S4 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.74',
     'username': 'brian',
-    'password': 'cisco',
+    'password': 'c1sc0',
 }
 
 access_S5 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.75',
     'username': 'brian',
-    'password': 'cisco',
+    'password': 'c1sc0',
 }
