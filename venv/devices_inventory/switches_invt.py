3d85ae7b344f8682489ce30f9ccc8e41480f661a
ndeva <ndevabn@gmail.com>
2021-01-06 22:35:26 +0300

    An inventory of the switches

---
+++ venv/devices_inventory/switches_invt.py
@@ -0,0 +1,34 @@
+core_S1 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.71',
+    'username': 'brian',
+    'password': 'cisco',
+}
+
+core_S2 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.72',
+    'username': 'brian',
+    'password': 'cisco',
+}
+
+access_S3 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.73',
+    'username': 'brian',
+    'password': 'cisco',
+}
+
+access_S4 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.74',
+    'username': 'brian',
+    'password': 'cisco',
+}
+
+access_S5 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.75',
+    'username': 'brian',
+    'password': 'cisco',
+}
